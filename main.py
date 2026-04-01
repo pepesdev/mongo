a63e585f977f871edda963066c1de321d40b71cb
Jose Hernandez Jaimes <jose.hernandez.jaime.contractor@bbva.com>
2026-03-31 23:01:02 -0600

    add method to find all users and find by email

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from model import User, get_connection
 
 def insert_sample_data():
     users_data = [
-        {"name":"Jose Hernandez","email":"hedzjames@gmail.com"},
+        {"name":"Jose Hernandez","email":"hedz@gmail.com"},
         {"name":"Berenice Alvarado","email":"bere_1234@gmail.com"}
     ]
     created_users=[]
@@ -13,6 +13,22 @@ def insert_sample_data():
     
     print(f"inserted {len(created_users)} users successfully")
 
+def get_all_users():
+    users = User.objects()
+    for user in users:
+        print(f"{user.name} - {user.email}")
+    return list(users)
+
+def get_user_by_email(email):
+    return User.objects(email=email).first()
+  
 if __name__ == "__main__":
     get_connection()
     insert_sample_data()
+    print("\n--- ALL USERS ---")
+    get_all_users()
+    
+    print("\n--- FIND ONE ---")
+    user = get_user_by_email("hedz@gmail.com")
+    if user:
+        print(user.name)
